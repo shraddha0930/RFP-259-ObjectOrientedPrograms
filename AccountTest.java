d4dcda98fb1f0f18facdbe5cf3040aad14ce843f
shraddha0930 <shraddhaingole11@gmail.com>
2023-03-06 18:52:14 +0530

    Added:code to calculate the money that withdraw from an account

---
+++ AccountTest.java
@@ -0,0 +1,12 @@
+package assignments.ObjectOrientedPrograms;
+
+import java.util.Scanner;
+
+public class AccountTest {
+    public static void main(String[] args) {
+        Account account  =new Account();
+        account.withdraw();
+
+    }
+}
+
